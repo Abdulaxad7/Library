4eb643cbc38b93cf4596661af1721e21902a3cc1
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2024-04-11 20:31:56 +0500

    Library

--- Screen.cpp
+++ Screen.cpp
@@ -14,22 +14,22 @@ const std::string BG="\033[40m";
 
 
 void Screen::welcome() {
-    std::cout << GREEN <<std::setw(41)<<BG<< "\tW         W\t"<< "EEEEE \t"<< "  L     \t"<< "  CCCC \t\t"<< "  OOO  \t\t"<< "M         M\t\t"<< "EEEEE\t "<< RESET << std::endl;
-    std::cout << GREEN <<std::setw(41)<<BG<< "\tW         W\t"<< "E     \t"<< "  L     \t"<< " C     \t\t"<< " O   O \t\t"<< "MM       MM\t\t"<< "E    \t "<< RESET << std::endl;
-    std::cout << GREEN <<std::setw(41)<<BG<< "\tWW   W   WW\t"<< "EEEE  \t"<< "  L     \t"<< "C      \t\t"<< "O     O\t\t"<< "M M     M M\t\t"<< "EEEE \t "<< RESET << std::endl;
-    std::cout << GREEN <<std::setw(41)<<BG<< "\tW  W   W  W\t"<< "E     \t"<< "  L     \t"<< " C     \t\t"<< " O   O \t\t"<< "M  M   M  M\t\t"<< "E    \t "<< RESET << std::endl;
-    std::cout << GREEN <<std::setw(41)<<BG<< "\tW W     W W\t"<< "EEEEE \t"<< "  LLLLL \t"<< "  CCCC \t\t"<< "  OOO  \t\t"<< "M   M M   M\t\t"<< "EEEEE\t "<< RESET << std::endl;
+    std::cout << GREEN <<std::setw(80)<<BG<< "\tW         W\t"<< "EEEEE \t"<< "  L     \t"<< "  CCCC \t\t"<< "  OOO  \t\t"<< "M         M\t\t"<< "EEEEE\t "<< RESET << std::endl;
+    std::cout << GREEN <<std::setw(80)<<BG<< "\tW         W\t"<< "E     \t"<< "  L     \t"<< " C     \t\t"<< " O   O \t\t"<< "MM       MM\t\t"<< "E    \t "<< RESET << std::endl;
+    std::cout << GREEN <<std::setw(80)<<BG<< "\tWW   W   WW\t"<< "EEEE  \t"<< "  L     \t"<< "C      \t\t"<< "O     O\t\t"<< "M M     M M\t\t"<< "EEEE \t "<< RESET << std::endl;
+    std::cout << GREEN <<std::setw(80)<<BG<< "\tW  W   W  W\t"<< "E     \t"<< "  L     \t"<< " C     \t\t"<< " O   O \t\t"<< "M  M   M  M\t\t"<< "E    \t "<< RESET << std::endl;
+    std::cout << GREEN <<std::setw(80)<<BG<< "\tW W     W W\t"<< "EEEEE \t"<< "  LLLLL \t"<< "  CCCC \t\t"<< "  OOO  \t\t"<< "M   M M   M\t\t"<< "EEEEE\t "<< RESET << std::endl;
 
-    std::cout<< BLUE <<std::setw(69)<<BG<<"\tTTTTTTTTT\t"  << "  OOO  \t\t"<< RESET << std::endl;
-    std::cout<< BLUE <<std::setw(69)<<BG<<"\t    T    \t"  << " O   O \t\t"<< RESET << std::endl;
-    std::cout<< BLUE <<std::setw(69)<<BG<<"\t    T    \t"  << "O     O\t\t"<< RESET << std::endl;
-    std::cout<< BLUE <<std::setw(69)<<BG<<"\t    T    \t"  << " O   O \t\t"<< RESET << std::endl;
-    std::cout<< BLUE <<std::setw(69)<<BG<<"\t    T    \t"  << "  OOO  \t\t"<< RESET << std::endl;
-     std::cout<<RED << std::setw(41)<<BG<<"\tL     \t"<< "  IIIII  \t"<< "  BBBBB  \t"<< "  RRRRR  \t"<< "    A    \t"<< "  RRRRR  \t"<< "Y     Y \t"<<RESET<<std::endl ;
-     std::cout<<RED << std::setw(41)<<BG<<"\tL     \t"<< "    I    \t"<< "  B    B \t"<< "  R    R \t"<< "   A A   \t"<< "  R    R \t"<< " Y   Y  \t"<<RESET<<std::endl ;
-     std::cout<<RED << std::setw(41)<<BG<<"\tL     \t"<< "    I    \t"<< "  BBBBB  \t"<< "  RRRRR  \t"<< "  AAAAA  \t"<< "  RRRRR  \t"<< "  Y Y   \t"<<RESET<<std::endl ;
-     std::cout<<RED << std::setw(41)<<BG<<"\tL     \t"<< "    I    \t"<< "  B    B \t"<< "  R   R  \t"<< " A     A \t"<< "  R   R  \t"<< "   Y    \t"<<RESET<<std::endl ;
-     std::cout<<RED << std::setw(41)<<BG<<"\tLLLLL \t"<< "  IIIII  \t"<< "  BBBBB  \t"<< "  R    R \t"<< "A       A\t"<< "  R    R \t"<< "   Y    \t"<<RESET<<std::endl ;
+    std::cout<< BLUE <<std::setw(108)<<BG<<"\tTTTTTTTTT\t"  << "  OOO  \t\t"<< RESET << std::endl;
+    std::cout<< BLUE <<std::setw(108)<<BG<<"\t    T    \t"  << " O   O \t\t"<< RESET << std::endl;
+    std::cout<< BLUE <<std::setw(108)<<BG<<"\t    T    \t"  << "O     O\t\t"<< RESET << std::endl;
+    std::cout<< BLUE <<std::setw(108)<<BG<<"\t    T    \t"  << " O   O \t\t"<< RESET << std::endl;
+    std::cout<< BLUE <<std::setw(108)<<BG<<"\t    T    \t"  << "  OOO  \t\t"<< RESET << std::endl;
+     std::cout<<RED << std::setw(80)<<BG<<"\tL     \t"<< "  IIIII  \t"<< "  BBBBB  \t"<< "  RRRRR  \t"<< "    A    \t"<< "  RRRRR  \t"<< "Y     Y \t"<<RESET<<std::endl ;
+     std::cout<<RED << std::setw(80)<<BG<<"\tL     \t"<< "    I    \t"<< "  B    B \t"<< "  R    R \t"<< "   A A   \t"<< "  R    R \t"<< " Y   Y  \t"<<RESET<<std::endl ;
+     std::cout<<RED << std::setw(80)<<BG<<"\tL     \t"<< "    I    \t"<< "  BBBBB  \t"<< "  RRRRR  \t"<< "  AAAAA  \t"<< "  RRRRR  \t"<< "  Y Y   \t"<<RESET<<std::endl ;
+     std::cout<<RED << std::setw(80)<<BG<<"\tL     \t"<< "    I    \t"<< "  B    B \t"<< "  R   R  \t"<< " A     A \t"<< "  R   R  \t"<< "   Y    \t"<<RESET<<std::endl ;
+     std::cout<<RED << std::setw(80)<<BG<<"\tLLLLL \t"<< "  IIIII  \t"<< "  BBBBB  \t"<< "  R    R \t"<< "A       A\t"<< "  R    R \t"<< "   Y    \t"<<RESET<<std::endl ;
 
 
 //        for (int i=0;i<101;i++) {
